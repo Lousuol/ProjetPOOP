7339c339fbd8ebcd179aeeace20e5131bc71505f
Elisa <162178990+Lousuol@users.noreply.github.com>
2026-02-24 11:14:30 +0100

    Update texte.py

--- src/texte.py
+++ src/texte.py
@@ -1,3 +1,6 @@
+from collections import Counter
+import re
+
 class Texte : # document littéraire avec ses métadonnées et des méthodes d'analyses simples 
   genre_par_defaut = "Texte" # attribut de CLASSE
   def __init__ (self, titre: str, auteur: str, contenu: str, annee: int):
@@ -5,16 +8,33 @@ class Texte : # document littéraire avec ses métadonnées et des méthodes d'a
     self.auteur = auteur
     self.contenu = contenu
     self.annee = annee
-    self.chapitres : list [str] = []
+ 
   def nombres_mots(self) -> int :
-    return sum (len(ch.split()) for ch in self.chapitres)
+    return sum (len(c.split()) for c in self.contenu)
+  
   def mots_uniques(self) -> set[str] : 
-  def frequence(self) -> dict[str, int]
+    counter = Counter(self.contenu.lower().split())
+    return {mot for mot in counter if counter[mot] == 1}
+
+  
+  def frequence(self) -> dict[str, int] :
+    words = re.findall(r'\w+', open('texte02.txt').read().lower())
+    Counter(words).most_common()
+    return {mot: self.contenu.count(mot) for mot in set (self.contenu.split())}
+    
   @property
   def titre (self) -> str :
     return self._titre
+    
   @titre.setter
   def titre (self, nouveau: str) -> None :
     if not nouveau.strip () :
-      raise ValueError ("Le titre ne peut pas etre vide ")
+      raise ValueError ("Le titre ne peut pas etre vide")
     self._titre = nouveau.strip()
+
+andros = Texte("The woman of andros", "Wilder", "The earth sighed as it turned in its course; the shadow of night crept gradually along the Mediterranean, and Asia was left in darkness.", 1930)
+
+print(andros.nombres_mots())
+print(andros.mots_uniques())
+print(andros.frequence())
+print(andros.genre_par_defaut)
